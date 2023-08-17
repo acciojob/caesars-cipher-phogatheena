cfc6afc87aec38240a28780904bf8b7c12ad283c
phogatheena <109434462+phogatheena@users.noreply.github.com>
2023-08-17 22:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ function rot13(encodedStr) {
     const decodedChar = lookup[char] || char; // If not found in lookup, use the same character
 
     decodedArr.push(decodedChar);
-  }
+    var a = decodedArr.toString();
 
-  return decodedArr.join(""); //return decodedArr
+  return decodedArr.toString().replace(/,/g, "");//decodedArr.join(""); //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
